d090430554e052c2353e7c0b2ec4d726c0885767
Andreas Flöjt <andenzz@hotmail.com>
2016-01-25 17:29:27 +0100

    Mesh releated functionality. Not yet tested

--- Radiant/Radiant/Graphics.h
+++ Radiant/Radiant/Graphics.h
@@ -12,6 +12,8 @@
 ////////////////////
 #include "Direct3D11.h"
 #include "IRenderProvider.h"
+#include "Mesh.h"
+#include "Utils.h"
 
 #pragma comment (lib, "d3d11.lib")
 
@@ -28,8 +30,9 @@ public:
 
 	void AddRenderProvider( IRenderProvider *provider );
 
-private:
+	bool CreateBuffers( Mesh *mesh, std::uint32_t& vertexBufferIndex, std::uint32_t& indexBufferIndex );
 
+private:
 	HRESULT OnCreateDevice( void );
 	void OnDestroyDevice( void );
 	HRESULT OnResizedSwapChain( void );
@@ -37,6 +40,10 @@ private:
 
 	void BeginFrame( void );
 	void EndFrame( void );
+
+	void _InterleaveVertexData( Mesh *mesh, void **vertexData, std::uint32_t& vertexDataSize, void **indexData, std::uint32_t& indexDataSize );
+	ID3D11Buffer* _CreateVertexBuffer( void *vertexData, std::uint32_t vertexDataSize );
+	ID3D11Buffer* _CreateIndexBuffer( void *indexData, std::uint32_t indexDataSize );
 
 private:
 	Direct3D11 *_D3D11 = nullptr;
@@ -46,6 +53,9 @@ private:
 	// Elements are submitted by render providers, and is cleared on every
 	// frame. It's a member variable to avoid reallocating memory every frame.
 	std::vector<RenderJob> _Meshes;
+
+	std::vector<ID3D11Buffer*> _VertexBuffers;
+	std::vector<ID3D11Buffer*> _IndexBuffers;
 };
 
 #endif
