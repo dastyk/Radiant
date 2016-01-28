b8281d2d84acd5daf63b80bb263551b819617bff
jimnilsson <jimnilssonteit3@gmail.com>
2016-01-28 17:29:24 +0100

    Nothing.

--- Radiant/Radiant/MaterialManager.cpp
+++ Radiant/Radiant/MaterialManager.cpp
@@ -13,10 +13,11 @@ MaterialManager::~MaterialManager()
 void MaterialManager::CreateMaterial(Entity entity, const std::string& shaderName)
 {
 	
-
+	
 }
 
-void MaterialManager::SetFloat(Entity entity, const std::string & materialProperty, float value)
+void MaterialManager::SetFloat(Entity entity, const std::string & materialProperty, float value, uint32_t subMesh)
 {
-	_entityToMaterial[entity]
+	
+
 }
--- Radiant/Radiant/MaterialManager.h
+++ Radiant/Radiant/MaterialManager.h
@@ -5,6 +5,7 @@
 #include <DirectXMath.h>
 #include "Entity.h"
 #include "FileHandler.h"
+#include "System.h"
 #include <cstdint>
 #include <vector>
 #include <unordered_map>
@@ -17,7 +18,7 @@ public:
 	~MaterialManager();
 
 	void CreateMaterial(Entity entity, const std::string& shaderName);
-	void SetFloat(Entity entity, const std::string& materialProperty, float value);
+	void SetFloat(Entity entity, const std::string& materialProperty, float value, uint32_t subMesh = 0);
 
 
 
@@ -25,10 +26,16 @@ private:
 	struct MapAndMemory
 	{
 		std::unordered_map<std::string, uint32_t> _nameToIndex;
+		uint32_t _size;
 		float* _memory;
 	};
-	std::vector<std::string> _processedShaders;
-	std::unordered_map<Entity, std::vector<MapAndMemory>> _entityToMaterial;
+	struct SizeAndMapping
+	{
+		uint32_t _size;
+		std::unordered_map<std::string, uint32_t> _nameToindex;
+	};
+	std::unordered_map<std::string, SizeAndMapping> _shaderNameToSizeAndMapping;
+	std::unordered_map<Entity, std::unordered_map<uint32_t, MapAndMemory>> _entityToMaterial;
 	
 };
 
