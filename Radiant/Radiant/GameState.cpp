38a0ef7b15f899c306ceeeb35c8c5b83ec9e369b
Andreas Jonasson <andreas.jonasson1993@gmail.com>
2016-02-02 16:32:52 +0100

    Tweaking the light. Something is fishy. Getting Flöjt.

--- Radiant/Radiant/GameState.cpp
+++ Radiant/Radiant/GameState.cpp
@@ -42,7 +42,7 @@ void GameState::Init()
 	//====		Create Lists		====
 	//==================================
 	_enemies->AddElementToList(new Enemy(_managers->CreateObject(
-		XMVectorSet(0.0f, -12.5f, 0.0f, 0.0f),
+		XMVectorSet(0.0f, -15.0f, 0.0f, 0.0f),
 		XMVectorSet(0.0f, 0.0f, 0.0f, 0.0f),
 		XMVectorSet(25.0f, 1.0f, 25.0f, 0.0f),
 		"Assets/Models/cube.arf",
@@ -53,16 +53,16 @@ void GameState::Init()
 	_enemies->AddElementToList(new Enemy(_managers->CreateObject(
 		XMVectorSet(-10.0f, -10.0f, 10.0f, 0.0f),
 		XMVectorSet(0.0f, 0.0f, 0.0f, 0.0f),
-		XMVectorSet(5.0f, 5.0f, 5.0f, 0.0f),
+		XMVectorSet(1.0f, 1.0f, 1.0f, 0.0f),
 		"Assets/Models/cube.arf",
 		"Assets/Textures/stonetex.dds",
 		"Assets/Textures/stonetexnormal.dds")), 1);
 	_managers->material->SetMaterialProperty(_enemies->GetCurrentElement()->GetEntity(), 0, "Roughness", 1.0f, "Shaders/GBuffer.hlsl");
-	_managers->light->BindPointLight(_enemies->GetCurrentElement()->GetEntity(), XMFLOAT3(-10.0f, -10.0f, 10.0f), 100.0, XMFLOAT3(1.0f, 1.0f, 1.0f), 10.0f);
+	_managers->light->BindPointLight(_enemies->GetCurrentElement()->GetEntity(), XMFLOAT3(0.0f, 0.0f, 0.0f), 100.0, XMFLOAT3(1.0f, 1.0f, 1.0f), 100.0f);
 	_managers->transform->RotateRoll(_enemies->GetCurrentElement()->GetEntity(), 0.0f);
 
 	_enemies->AddElementToList(new Enemy(_managers->CreateObject(
-		XMVectorSet(-12.0f, 0.0f, 0.0f, 0.0f),
+		XMVectorSet(-20.0f, 0.0f, 0.0f, 0.0f),
 		XMVectorSet(0.0f, 0.0f, 0.0f, 0.0f),
 		XMVectorSet(1.0f, 25.0f, 25.0f, 0.0f),
 		"Assets/Models/cube.arf",
@@ -72,7 +72,7 @@ void GameState::Init()
 	_managers->transform->RotateRoll(_enemies->GetCurrentElement()->GetEntity(), 0.0f);
 
 	_enemies->AddElementToList(new Enemy(_managers->CreateObject(
-		XMVectorSet(12.0f, 0.0f, 0.0f, 0.0f),
+		XMVectorSet(20.0f, 0.0f, 0.0f, 0.0f),
 		XMVectorSet(0.0f, 0.0f, 0.0f, 0.0f),
 		XMVectorSet(1.0f, 25.0f, 25.0f, 0.0f),
 		"Assets/Models/cube.arf",
@@ -82,7 +82,7 @@ void GameState::Init()
 	_managers->transform->RotateRoll(_enemies->GetCurrentElement()->GetEntity(), 0.0f);
 
 	_enemies->AddElementToList(new Enemy(_managers->CreateObject(
-		XMVectorSet(0.0f, 0.0f, -12.0f, 0.0f),
+		XMVectorSet(0.0f, 0.0f, -20.0f, 0.0f),
 		XMVectorSet(0.0f, 0.0f, 0.0f, 0.0f),
 		XMVectorSet(25.0f, 25.0f, 1.0f, 0.0f),
 		"Assets/Models/cube.arf",
@@ -92,7 +92,7 @@ void GameState::Init()
 	_managers->transform->RotateRoll(_enemies->GetCurrentElement()->GetEntity(), 0.0f);
 
 	_enemies->AddElementToList(new Enemy(_managers->CreateObject(
-		XMVectorSet(0.0f, 0.0f, 12.0f, 0.0f),
+		XMVectorSet(0.0f, 0.0f, 20.0f, 0.0f),
 		XMVectorSet(0.0f, 0.0f, 0.0f, 0.0f),
 		XMVectorSet(25.0f, 25.0f, 1.0f, 0.0f),
 		"Assets/Models/cube.arf",
@@ -152,7 +152,7 @@ void GameState::Update()
 
 	_gameTimer.Tick();
 	_test += _gameTimer.DeltaTime();
-	_managers->light->ChangePointLightRange(_enemies->GetElementByID(1)->GetEntity(), 10.0f*abs(sin(_test)));
+	_managers->light->ChangePointLightRange(_enemies->GetElementByID(1)->GetEntity(), 15.0f*abs(sin(_test)));
 
 	_player->Update(_gameTimer.DeltaTime());
 }
