8ca8bc08c1aafc5eb7286b62c808dc589e03d13e
dastyk <dastykill@gmail.com>
2016-02-03 22:09:00 +0100

    cpu access need to be set!

--- Radiant/Radiant/Graphics.cpp
+++ Radiant/Radiant/Graphics.cpp
@@ -321,54 +321,54 @@ const void Graphics::_BuildVertexData(FontData& data, TextVertexLayout* vertexPt
 
 	// Get the number of letters in the sentence.
 	numLetters = (uint)data.text.size();
-	vertexDataSize =sizeof(TextVertexLayout)* numLetters * 6;
+	vertexDataSize = sizeof(TextVertexLayout)* numLetters * 6;
 	vertexPtr = new TextVertexLayout[numLetters * 6];
 
 	// Initialize the index to the vertex array.
 	index = 0;
 
-		// Draw each letter onto a quad.
-		for (i = 0; i<numLetters; i++)
+	// Draw each letter onto a quad.
+	for (i = 0; i < numLetters; i++)
+	{
+		letter = ((int)data.text[i]) - 32;
+
+		// If the letter is a space then just move over three pixels.
+		if (letter == 0)
 		{
-			letter = ((int)data.text[i]) - 32;
-
-			// If the letter is a space then just move over three pixels.
-			if (letter == 0)
-			{
-				drawX = drawX + 3.0f;
-			}
-			else
-			{
-				// First triangle in quad.
-				vertexPtr[index]._position = XMFLOAT3(drawX, drawY, 0.0f);  // Top left.
-				vertexPtr[index]._texCoords = XMFLOAT2(data.font->Font[letter].left, 0.0f);
-				index++;
-
-				vertexPtr[index]._position = XMFLOAT3((drawX + data.font->Font[letter].size), (drawY - 16), 0.0f);  // Bottom right.
-				vertexPtr[index]._texCoords = XMFLOAT2(data.font->Font[letter].right, 1.0f);
-				index++;
-
-				vertexPtr[index]._position = XMFLOAT3(drawX, (drawY - 16), 0.0f);  // Bottom left.
-				vertexPtr[index]._texCoords = XMFLOAT2(data.font->Font[letter].left, 1.0f);
-				index++;
-
-				// Second triangle in quad.
-				vertexPtr[index]._position = XMFLOAT3(drawX, drawY, 0.0f);  // Top left.
-				vertexPtr[index]._texCoords = XMFLOAT2(data.font->Font[letter].left, 0.0f);
-				index++;
-
-				vertexPtr[index]._position = XMFLOAT3(drawX + data.font->Font[letter].size, drawY, 0.0f);  // Top right.
-				vertexPtr[index]._texCoords = XMFLOAT2(data.font->Font[letter].right, 0.0f);
-				index++;
-
-				vertexPtr[index]._position = XMFLOAT3((drawX + data.font->Font[letter].size), (drawY - 16), 0.0f);  // Bottom right.
-				vertexPtr[index]._texCoords = XMFLOAT2(data.font->Font[letter].right, 1.0f);
-				index++;
-
-				// Update the x location for drawing by the size of the letter and one pixel.
-				drawX = drawX + data.font->Font[letter].size + 1.0f;
-			}
+			drawX = drawX + 3.0f;
 		}
+		else
+		{
+			// First triangle in quad.
+			vertexPtr[index]._position = XMFLOAT3(drawX, drawY, 0.0f);  // Top left.
+			vertexPtr[index]._texCoords = XMFLOAT2(data.font->Font[letter].left, 0.0f);
+			index++;
+
+			vertexPtr[index]._position = XMFLOAT3((drawX + data.font->Font[letter].size), (drawY - 16), 0.0f);  // Bottom right.
+			vertexPtr[index]._texCoords = XMFLOAT2(data.font->Font[letter].right, 1.0f);
+			index++;
+
+			vertexPtr[index]._position = XMFLOAT3(drawX, (drawY - 16), 0.0f);  // Bottom left.
+			vertexPtr[index]._texCoords = XMFLOAT2(data.font->Font[letter].left, 1.0f);
+			index++;
+
+			// Second triangle in quad.
+			vertexPtr[index]._position = XMFLOAT3(drawX, drawY, 0.0f);  // Top left.
+			vertexPtr[index]._texCoords = XMFLOAT2(data.font->Font[letter].left, 0.0f);
+			index++;
+
+			vertexPtr[index]._position = XMFLOAT3(drawX + data.font->Font[letter].size, drawY, 0.0f);  // Top right.
+			vertexPtr[index]._texCoords = XMFLOAT2(data.font->Font[letter].right, 0.0f);
+			index++;
+
+			vertexPtr[index]._position = XMFLOAT3((drawX + data.font->Font[letter].size), (drawY - 16), 0.0f);  // Bottom right.
+			vertexPtr[index]._texCoords = XMFLOAT2(data.font->Font[letter].right, 1.0f);
+			index++;
+
+			// Update the x location for drawing by the size of the letter and one pixel.
+			drawX = drawX + data.font->Font[letter].size + 1.0f;
+		}
+	}
 
 	return;
 }
