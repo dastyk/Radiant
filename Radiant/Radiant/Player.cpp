de4c8a9239d72b2fb38b1f93ad26cbd01dc288d5
Joakim Ståhle-Nilsson <joakimthegamer@yahoo.se>
2016-02-10 10:15:35 +0100

    map thing

--- Radiant/Radiant/Player.cpp
+++ Radiant/Radiant/Player.cpp
@@ -198,7 +198,7 @@ Player::Player(EntityBuilder* builder) : _builder(builder)
 	_builder->Material()->SetMaterialProperty(_camera, 0, "Roughness", 1.0f, "Shaders/GBuffer.hlsl");
 	_builder->Material()->SetMaterialProperty(_camera, 0, "Metalic", 0.1f, "Shaders/GBuffer.hlsl");
 	_builder->Bounding()->CreateBoundingBox(_camera, _builder->Mesh()->GetMesh(_camera));
-	_builder->GetEntityController()->Transform()->MoveForward(_camera, 0);
+	_builder->GetEntityController()->Transform()->MoveForward(_camera, -1);
 
 }
 
