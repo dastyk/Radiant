7290d3a7c6ca66a15d495cb0d929842c5daf8a41
Joakim Ståhle-Nilsson <joakimthegamer@yahoo.se>
2016-03-08 15:35:55 +0100

    added base for light thrower weapon

---
+++ Radiant/Radiant/LightThrowerWeapon.cpp
@@ -0,0 +1,75 @@
+#include "LightThrowerWeapon.h"
+#include "System.h"
+
+LightThrowerWeapon::LightThrowerWeapon(EntityBuilder* builder, Entity player) : Weapon(builder, 3)
+{
+	_timeSinceLastActivation = 100;
+	_cooldown = 0.05f;
+	_fire = false;
+	_maxAmmo = 150;
+	_currentAmmo = 150;
+
+	_builder->Bounding()->CreateBoundingSphere(_weaponEntity, 0.05f);
+
+	_builder->Light()->BindPointLight(_weaponEntity, XMFLOAT3(0, 0, 0), 0.1f, XMFLOAT3(1.0f, 165.0f / 255.0f, 0.0f), 5);
+	_builder->Light()->ChangeLightBlobRange(_weaponEntity, 0.1f);
+	_builder->Transform()->BindChild(player, _weaponEntity);
+
+	_moveVector = XMFLOAT3(0.0f, 0.0f, 1.0f);
+
+	_active = true;
+}
+
+LightThrowerWeapon::~LightThrowerWeapon()
+{
+
+}
+
+void LightThrowerWeapon::Update(Entity playerEntity, float deltaTime)
+{
+	_timeSinceLastActivation += deltaTime;
+
+	for (int i = 0; i < _projectiles.size(); i++)
+	{
+		_projectiles[i]->Update(deltaTime);
+	}
+
+	for (int i = 0; i < _projectiles.size(); i++)
+	{
+		if (!_projectiles[i]->GetState())
+		{
+			delete _projectiles[i];
+			_projectiles.erase(_projectiles.begin() + i);
+			_projectiles.shrink_to_fit();
+			i--;
+		}
+	}
+
+	if (_fire == true)
+	{
+		_projectiles.push_back(new RapidFireProjectile(playerEntity, _builder, 1.0f));
+		_fire = false;
+
+	}
+
+}
+
+void LightThrowerWeapon::Shoot()
+{
+	if (System::GetInput()->IsMouseKeyDown(VK_LBUTTON) && HasAmmo())
+		this->_Shoot();
+}
+
+void LightThrowerWeapon::_Shoot()
+{
+	if (_cooldown - _timeSinceLastActivation <= 0)
+	{
+		_fire = true;
+		_currentAmmo -= 1;
+		//_builder->Light()->ChangeLightBlobRange(_weaponEntity, 0.1f*(_currentAmmo / (float)_maxAmmo));
+		_builder->Animation()->PlayAnimation(_weaponEntity, "scale", 0.1f*(_currentAmmo / (float)_maxAmmo) - _currentSize);
+		System::GetAudio()->PlaySoundEffect(L"basicattack.wav", 0.15f);
+
+		_timeSinceLastActivation = 0.0;
+	}
+}
